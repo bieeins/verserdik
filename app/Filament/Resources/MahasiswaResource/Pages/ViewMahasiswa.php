21ad67539646a633026e4910e1f70c0269a3f8af
bieeins <bieeins@gmail.com>
2025-02-03 15:46:31 +0700

    update new

--- app/Filament/Resources/MahasiswaResource/Pages/ViewMahasiswa.php
+++ app/Filament/Resources/MahasiswaResource/Pages/ViewMahasiswa.php
@@ -9,4 +9,19 @@ use Filament\Resources\Pages\ViewRecord;
 class ViewMahasiswa extends ViewRecord
 {
     protected static string $resource = MahasiswaResource::class;
+
+    // protected function getActions(): array
+    // {
+    //     return [
+    //         Actions\Action::make('linkFinalSertifikat')
+    //             ->label('Lihat Sertifikat')
+    //             ->url($this->record->linkFinalSertifikat)
+    //             ->target('_blank'),
+    //     ];
+    // }
+
+    // public function getLinkFinalSertifikatAttribute()
+    // {
+    //     return $this->record->linkFinalSertifikat;
+    // }
 }
